5154d5d61e8ad9614f54381f6c549024e222f4da
sunlu <sunlu.electric@gmail.com>
2026-05-24 21:06:26 +1000

    revised statistics hypothesis test

--- A Notebook on Probability, Statistics and Data Science/chapters/part-2/codes/plotfigs.m
+++ A Notebook on Probability, Statistics and Data Science/chapters/part-2/codes/plotfigs.m
@@ -1,9 +1,11 @@
 % plotfigs.m
+significance_level = 0.01;
+u_alpha = sqrt(2) * erfinv(1-2*significance_level);
 x = -5:0.01:5;
 y = normpdf(x);
-x1 = 2.58:0.01:5;
+x1 = u_alpha:0.01:5;
 y1 = normpdf(x1);
-x2 = -5:0.01:2.58;
+x2 = -5:0.01:u_alpha;
 y2 = normpdf(x2);
 figure
 ax = subplot(1,1,1);
@@ -11,7 +13,7 @@ hold on
 plot(ax,x,y,'k-', 'linewidth', 2)
 patch([x2 fliplr(x2)], [zeros(size(x2)) fliplr(y2)], 'g')
 patch([x1 fliplr(x1)], [zeros(size(x1)) fliplr(y1)], 'r')
-xline(2.58)
-xlabel('$\mu-\bar{X}$','interpreter','latex')
-ylabel('$\mathcal{N}\left(\mu; \bar{X}, {\sigma}^2\right)$','interpreter','latex')
+xline(u_alpha)
+xlabel('$\frac{\sigma}{\sqrt{m}}\left(\mu - \bar{X}\right)$','interpreter','latex')
+ylabel('$\mathcal{N}\left(0,1\right)$','interpreter','latex')
 grid on
